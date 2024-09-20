6d2ce079b591947deb806bdcb66ed175f9a862da
Meltyril <111682264+Meltyril@users.noreply.github.com>
2024-09-20 16:16:38 +0700

    Update ListFragment.kt

--- app/src/main/java/com/example/week04/ListFragment.kt
+++ app/src/main/java/com/example/week04/ListFragment.kt
@@ -1,8 +1,18 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.week04.R
+
 class ListFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
@@ -25,9 +35,5 @@ class ListFragment : Fragment() {
                 )
             )
         }
-    }
-
-    companion object {
-        const val COFFEE_ID = "COFFEE_ID"
     }
 }
